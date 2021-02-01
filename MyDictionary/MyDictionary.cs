f1c3c742dd226949a734a11101b2767c1abdafd3
OduNccu <71985511+OduNccu@users.noreply.github.com>
2021-02-02 00:01:40 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<KeyType, ValueType>
+    {
+        KeyType[] _keyArray;
+        ValueType[] _valueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new KeyType[0];
+            _valueArray = new ValueType[0];
+
+        }
+        public void Add(KeyType key, ValueType value)
+        {
+            KeyType[] _tempKeyArray = _keyArray;
+            _keyArray = new KeyType[_keyArray.Length + 1];
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = _tempKeyArray[i];
+            }
+            _keyArray[_keyArray.Length - 1] = key;
+
+
+
+            ValueType[] _tempValueArray = _valueArray;
+            _valueArray = new ValueType[_valueArray.Length + 1];
+            for (int i = 0; i < _tempValueArray.Length; i++)
+            {
+                _valueArray[i] = _tempValueArray[i];
+            }
+            _valueArray[_valueArray.Length - 1] = value;
+
+
+        }
+
+
+        public KeyType[] Key
+        {
+            get { return _keyArray; }
+        }
+        public ValueType[] Value
+        {
+            get { return _valueArray; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> user = new MyDictionary<int, string>();
+            user.Add(1, "Hamza");
+            user.Add(3, "Serhat");
+            user.Add(4, "Melisa");
+
+            Console.WriteLine("Number: " + user.Key[0] + "\n" + "Name: " + user.Value[0]);
+
+        }
+    }
+}
